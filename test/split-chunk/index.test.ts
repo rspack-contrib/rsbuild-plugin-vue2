94e33480d55a868b47d2a4f69a8a732858737848
inori_lover <inori_lover@outlook.com>
2025-04-19 23:08:04 +0800

    fix: make vue split strategy follow the splitChunks.vue key

--- test/split-chunk/index.test.ts
+++ test/split-chunk/index.test.ts
@@ -3,27 +3,40 @@ import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { expect, test } from '@playwright/test';
 import { createRsbuild } from '@rsbuild/core';
-import { pluginVue2 } from '../../dist';
+import { type PluginVueOptions, pluginVue2 } from '../../dist';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 test('should split vue chunks correctly', async () => {
-  const rsbuild = await createRsbuild({
-    cwd: __dirname,
-    rsbuildConfig: {
-      plugins: [pluginVue2()],
-      output: {
-        filenameHash: false,
+  const buildRsbuild = (options?: PluginVueOptions) => {
+    return createRsbuild({
+      cwd: __dirname,
+      rsbuildConfig: {
+        plugins: [pluginVue2(options)],
+        output: {
+          filenameHash: false,
+        },
       },
-    },
-  });
+    });
+  };
 
+  // default true
+  let rsbuild = await buildRsbuild();
   await rsbuild.build();
-
-  const libVuePath = path.join(__dirname, 'dist', 'static/js/lib-vue.js');
+  let libVuePath = path.join(__dirname, 'dist', 'static/js/lib-vue.js');
   expect(existsSync(libVuePath)).toBeTruthy();
 
+  // manual turn off
+  rsbuild = await buildRsbuild({ splitChunks: { vue: false } });
   await rsbuild.build();
+  libVuePath = path.join(__dirname, 'dist', 'static/js/lib-vue.js');
+  expect(existsSync(libVuePath)).toBeFalsy();
+
+  // manual turn on
+  rsbuild = await buildRsbuild({ splitChunks: { vue: true, router: false } });
+  await rsbuild.build();
+  libVuePath = path.join(__dirname, 'dist', 'static/js/lib-vue.js');
+  expect(existsSync(libVuePath)).toBeTruthy();
 });
 
 test('should not split vue chunks when strategy is `all-in-one`', async () => {
